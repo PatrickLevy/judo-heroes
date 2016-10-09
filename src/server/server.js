6e2dc219a6470a51e692497637a8db41d901aa6b
PatrickLevy <pat.levy@ties.k12.mn.us>
2016-10-09 13:51:05 -0500

    wip

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,10 @@ import { match, RouterContext } from 'react-router';
 import routes from '../routes';
 import NotFoundPage from '../components/NotFoundPage';
 
+// Mongo stuff
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
 // initialize the server and configure support for ejs templates
 const app = new Express();
 const server = new Server(app);
@@ -60,3 +64,63 @@ server.listen(port, err => {
   }
   console.info(`Server running on http://localhost:${port} [${env}]`);
 });
+
+
+// Mongo
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+var myDatabase;
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+});
+
+
+// Insert into database
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+// Find all Documents
+export function findDocuments(query, callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(docs)
+      callback(docs);
+    });
+
+  });
+}
+
+
+
+// findDocuments("", function(err, res) {
+//   if(!err) {
+//     console.log(res);  
+//   }
+  
+// });
